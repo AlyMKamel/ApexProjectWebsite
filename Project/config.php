0eb75d14df8c0329b842cdcb99ba3fb2aef8986b
AlyMKamel <157958378+AlyMKamel@users.noreply.github.com>
2024-11-02 13:12:48 +0200

    Update config.php

--- Project/config.php
+++ Project/config.php
@@ -1,4 +1,4 @@
 <?php
-$con=mysqli_connect("localhost:typeyourPortNumber","root","")or die("Error Can't Reach The Server");
-$db=mysqli_select_db($con,"webproject")or die ("Can't Connect To The Database");
+$con=mysqli_connect("localhost:typeyourPortNumber","UserName default for xampp is root","Password default for xampp is'empty- no password'")or die("Error Can't Reach The Server");
+$db=mysqli_select_db($con,"databaseName")or die ("Can't Connect To The Database");
 ?>
