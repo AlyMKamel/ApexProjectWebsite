b2b16df7a8e7593624c57a48aad60a30c0237b25
AlyMKamel <157958378+AlyMKamel@users.noreply.github.com>
2024-11-02 13:11:13 +0200

    Update config.php

--- Project/config.php
+++ Project/config.php
@@ -1,4 +1,4 @@
 <?php
-$con=mysqli_connect("localhost:3307","root","")or die("Error Can't Reach The Server");
+$con=mysqli_connect("localhost:typeyourPortNumber","root","")or die("Error Can't Reach The Server");
 $db=mysqli_select_db($con,"webproject")or die ("Can't Connect To The Database");
 ?>
